46de2334f532fea35ce9f4d67fe3cc2e7da6300a
Conrad H. Carl <coheca0815@gmail.com>
2024-03-30 16:41:31 +0100

    fix off by one

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ fn main() {
                 buffer[i] = 0; // Fill the first 512 bytes with 0
             }
             // copy font to 050-09F
-            buffer[FONT_BASE_ADDRESS..=FONT_BASE_ADDRESS + 80].copy_from_slice(&FONT);
+            buffer[FONT_BASE_ADDRESS..FONT_BASE_ADDRESS + 80].copy_from_slice(&FONT);
 
             println!("{:?}", buffer);
 
